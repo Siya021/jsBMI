ca0577db6133be9adfaff92e89c2121eed6f6901
Siya021 <siyamthandamazokwana@gmail.com>
2023-05-23 16:17:10 +0200

    BMI

---
+++ script/code.js
@@ -0,0 +1,17 @@
+function calculateBMI() {
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value / 100;
+
+  
+    if (weight === "" || height === "") {
+      document.getElementById("answer").innerHTML = "Please enter the missing values";
+    } else {
+      let bmi = weight / (height * height);
+      document.getElementById("answer").innerHTML = "Answer is =" + bmi.toFixed(2);
+    }}
+
+function clearValue() {
+    document.getElementById("weight").value = "";
+    document.getElementById("height").value = "";
+    document.getElementById("answer").innerHTML = "";
+}
